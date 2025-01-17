7e308e6ef66a513c84a856cc409a68f5a496ebb9
Marat <maratf76@gmail.com>
2025-01-17 17:19:57 +0500

    3

--- Program.cs
+++ Program.cs
@@ -1,24 +1,57 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
 
 namespace PostgresConnect1
 {
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<MyMessage> MyMessages { get; set; }
+        public DbSet<Alternative> Alternatives { get; set; }
+
+
         public ApplicationContext()
         {
-            Database.EnsureCreated();
+           // Database.EnsureDeleted();
+          //  Database.EnsureCreated();
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=password");
         }
     }
-    public class User
+    public class User    
     {
+        [Newtonsoft.Json.JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+    }
+
+    public class MyMessage
+    {
+        [Newtonsoft.Json.JsonIgnore]
+        public int Id { get; set; }
+        public string role { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Alternative
+    {
+        [Newtonsoft.Json.JsonIgnore]
+        public int Id { get; set; }
+        public MyMessage message { get; set; }
+        public string status { get; set; }
+    }
+
+    public class CompletionOptions
+    {
+        [Newtonsoft.Json.JsonIgnore]
+        public int Id { get; set; }
+        public bool stream { get; set; }
+        public float temperature { get; set; }
+        public int maxTokens { get; set; }
     }
 
 
@@ -42,26 +75,29 @@ namespace PostgresConnect1
             using (ApplicationContext db = new ApplicationContext())
             {
                 // обновляем только объекты, у которых имя Tom
-                db.Users.Where(u => u.Name == "Tom")
+                /*db.Users.Where(u => u.Name == "Tom")
                     .ExecuteUpdate(s => s
                             .SetProperty(u => u.Age, u => u.Age + 1)    // Age = Age + 1
                             .SetProperty(u => u.Name, u => "Tomas"));      // Name = "Tomas
-                
+                */
                 // получаем объекты из бд и выводим на консоль
                 var users = db.Users.ToList();
+
+                string t111 = JsonConvert.SerializeObject(users, Formatting.Indented);
+
                 Console.WriteLine("Users list before:");
                 foreach (User u in users)
                 {
                     Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
                 }
 
-                foreach (User u in users)
+               /* foreach (User u in users)
                 {
                     if (u.Age>30)
                         db.Users.Remove(u);
                 }
 
-                db.SaveChanges();
+                db.SaveChanges();*/
                 users = db.Users.ToList();
 
                 Console.WriteLine("Users list after:");
